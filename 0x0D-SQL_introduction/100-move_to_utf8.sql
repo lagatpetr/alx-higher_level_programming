df5a2e9d9ab11b7cfdbfa4f21e00dd67460fef89
lagatpetr <lagatpetr@gmail.com>
2023-08-15 09:57:05 -0700

    updated sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
--- Change the character encoding of the entire database hbtn_0c_0 to UTF-8.
+-- Update character encoding for the database hbtn_0c_0 to UTF-8.
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Change the character encoding of the table first_table to UTF-8.
+-- Update character encoding for the table first_table to UTF-8.
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
